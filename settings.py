a99d3d8f64eb15fd823ab9eadce2ffc0ca5e21c0
david-loe <d.loewens24@gmail.com>
2019-10-09 01:00:52 +0200

    index.HTML

--- settings.py
+++ settings.py
@@ -6,9 +6,13 @@ class Settings:
     day_range = relativedelta(months=0, days=7)
     staringpoint = relativedelta(months=0, days=0, hours=2, minutes=0) # time from where on to look for events (today + startingpoint)
     weekdays = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
-    resolution = [800,600]
+    ratios = {"16:9" : (16,9), "4:3" : (4,3), "A4": (210,297)}
+    ratio = "4:3"
     path_background = "" #if no background is used: ""
     path_qrcode = "" #if no qr-code is used: ""
     path_logo = "" #if no logo is used: ""
+    path_htmlOutput = ""
+    path_output = ""
+    name_output = ""
 
 
