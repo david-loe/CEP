683d034916030d636e28a16206d85f487f4e48a2
david-loe <d.loewens24@gmail.com>
2019-10-09 02:04:39 +0200

    added pdfsizes

--- htmlGen.py
+++ htmlGen.py
@@ -3,25 +3,33 @@ from ical import *
 from yattag import Doc
 
 
-#eventDaysList = getEventList()
-#count_days = len(eventDaysList)
+eventDaysList = getEventList()
+count_days = len(eventDaysList)
+pdfsize = set.paperformats[set.ratio]
 
 doc, tag, text = Doc().tagtext()
 doc.asis('<!DOCTYPE html>')
 
 with tag('html'):
     with tag('head'):
+        with tag('style'):
+            doc.asis('''@page {
+		    	    size: '''+ pdfsize +''' landscape;
+		            margin: 2cm;
+		            }''')
         doc.asis('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">')
     with tag('body'):
-#        with tag('div', style = "width: " +  str(set.resolution[0]) + "mm; height: " + str(set.resolution[1]) + "mm; background: 'light grey';"):
-            with tag('h1'):
+        with tag('div', klass='container'):
+            with tag('h1', align = "center"):
                 text('Terminübersicht')
-            with tag('p'):
-                text('Hier könnte ihre Werbung stehen')
+            with tag('div', klass='row justify-content-around'):
+                for day in eventDaysList:
+                    with tag('div',klass= 'col',  width = '30%'):
+                        for event in day:
+                            text(event.summary)
 
 
 file = open('index.HTML', 'w')
 file.write(doc.getvalue())
 file.close()
 
-print(set.ratios[set.ratio])
--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ class Settings:
     day_range = relativedelta(months=0, days=7)
     staringpoint = relativedelta(months=0, days=0, hours=2, minutes=0) # time from where on to look for events (today + startingpoint)
     weekdays = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
-    ratios = {"16:9" : (16,9), "4:3" : (4,3), "A4": (210,297)}
+    #ratios = {"16:9" : (16,9), "4:3" : (4,3), "A4": (210,297)}
+    paperformats = {"16:9": "B0", "4:3":"B1","A4":"A4"}
     ratio = "4:3"
     path_background = "" #if no background is used: ""
     path_qrcode = "" #if no qr-code is used: ""
