b51919ac2ba4dbb94770208a0c1e51ba2a957d79
lda7hi <david.loewens@de.bosch.com>
2019-12-09 15:35:48 +0100

    error handling open pdf

--- html2pdf.py
+++ html2pdf.py
@@ -3,7 +3,7 @@ from weasyprint import HTML, CSS
 from weasyprint.fonts import FontConfiguration
 
 pdfsize = ["35cm", (str(set.output_ratio[1] * (35 / set.output_ratio[0])) + "cm")]
-
+pdfName = set.name_output + '.pdf'
 
 font = FontConfiguration()
 html = HTML(filename=set.path_htmlOutput)
@@ -12,16 +12,19 @@ html = HTML(filename=set.path_htmlOutput)
 css_page = CSS(string='@page {size: ' + pdfsize[0] + ' ' + pdfsize[1] + '; margin = 0cm;}', font_config=font)
 
 
-""" 
-!TBD: Fehlerbehandlung, damit auch ein PFD erzeugt wird wenn die alte geöffnet ist. 
+
+#Fehlerbehandlung, damit auch ein PFD erzeugt wird wenn die alte geöffnet ist:
+#es wird fortlaufend _0# an den Namen angehängt
 try:
-    file_output = open(set.name_output + '.pdf', 'w')
+    file_output = open(pdfName, 'w')
 except:
     for i in range (1,10):
+        pdfName = set.name_output + '_0' + str(i) + '.pdf'
         try:
-            file_output = open(set.name_output + '_0' + str(i) + '.pdf', 'w')
+            file_output = open(pdfName, 'w')
         except:
             continue
-        break """
+        else:
+            break 
 
-html.write_pdf(target=set.name_output + '.pdf', stylesheets=[css_page], font_config=font)
+html.write_pdf(target=pdfName, stylesheets=[css_page], font_config=font)
